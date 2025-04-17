fb5e3b66404ab4d88aea2ca3548ba12498f06620
maxzdosreis <maxzdosreis@gmail.com>
2025-04-16 23:50:25 -0300

    Criação da Entidade FaixaCEP com os seus atributos e métodos

--- src/Parte1/FaixaCEP.java
+++ src/Parte1/FaixaCEP.java
@@ -1,4 +1,41 @@
 package Parte1;
 
 public class FaixaCEP {
+    private String cidade;
+    private int inicio;
+    private int fim;
+
+    public FaixaCEP(String cidade, int inicio, int fim) {
+        this.cidade = cidade;
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(int inicio) {
+        this.inicio = inicio;
+    }
+
+    public int getFim() {
+        return fim;
+    }
+
+    public void setFim(int fim) {
+        this.fim = fim;
+    }
+
+    public boolean contem(int cep){
+        return cep >= inicio && cep <= fim;
+    }
 }
