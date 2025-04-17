65653ca8388c9ddeefaa137e1936fce997c4b66f
maxzdosreis <maxzdosreis@gmail.com>
2025-04-17 00:01:00 -0300

    Criação do Código Principal do Desafio

--- src/Parte1/Main.java
+++ src/Parte1/Main.java
@@ -1,17 +1,31 @@
 package Parte1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws IOException {
+        List<FaixaCEP> faixas  = new ArrayList<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("entrada1.txt"))){
+            String linha;
+            while((linha = bufferedReader.readLine()) != null && !linha.equals("--")){
+                String[] partes = linha.split(",");
+                faixas.add(new FaixaCEP(partes[0], Integer.parseInt(partes[1]), Integer.parseInt(partes[2])));
+            }
+            String cepBusca = bufferedReader.readLine();
+            int cep = Integer.parseInt(cepBusca);
+            for (FaixaCEP faixa : faixas){
+                if (faixa.contem(cep)){
+                    System.out.println(faixa.getCidade());
+                    return;
+                }
+            }
+            System.out.println("Cep não encontrado");
         }
     }
 }
