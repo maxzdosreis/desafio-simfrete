9b38f85881e71f58130659a85c5acfad05b5381c
maxzdosreis <maxzdosreis@gmail.com>
2025-04-17 23:26:08 -0300

    Classe Aresta com variáveis e métodos Parte2

--- src/Parte2/Aresta.java
+++ src/Parte2/Aresta.java
@@ -1,4 +1,27 @@
 package Parte2;
 
 public class Aresta {
+    private String destino;
+    private double custo;
+
+    public Aresta(String destino, double custo) {
+        this.destino = destino;
+        this.custo = custo;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public double getCusto() {
+        return custo;
+    }
+
+    public void setCusto(double custo) {
+        this.custo = custo;
+    }
 }
