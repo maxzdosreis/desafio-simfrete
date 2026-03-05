0f1cd4a79e4d18a9f5e4860198cc5f414036e640
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2026-03-04 21:27:41 -0300

    feat: implementa cálculo de camihno mais curto com método de Dijkstra'

---
+++ src/services/GrafoService.ts
@@ -0,0 +1,81 @@
+import { Aresta } from "../models/Aresta";
+
+export class GrafoService {
+    private grafo: Map<string, Aresta[]>
+
+    constructor(arestas: { origem: string; destino: string; custo: number}[]) {
+        this.grafo = new Map()
+
+        for (const aresta of arestas) {
+            if (!this.grafo.has(aresta.origem)) {
+                this.grafo.set(aresta.origem, [])
+            }
+
+            this.grafo.get(aresta.origem)!.push({
+                destino: aresta.destino,
+                custo: aresta.custo
+            })
+        }
+    }
+
+    calcularMenorCaminho(origem: string, destino: string): {
+        rota: string[]
+        custoTotal: number
+    } | null {
+        const distancias = new Map<string, number>()
+        const anteriores = new Map<string, string | null>()
+        const fila: { cidade: string; custo: number}[] = []
+
+        for (const cidade of this.grafo.keys()) {
+            distancias.set(cidade, Infinity)
+            anteriores.set(cidade, null)
+        }
+
+        distancias.set(origem, 0)
+        fila.push({ cidade: origem, custo: 0})
+
+        while (fila.length > 0) {
+            fila.sort((a,b) => a.custo - b.custo)
+
+            const atual = fila.shift()!
+            const cidadeAtual = atual.cidade
+
+            if (cidadeAtual === destino) {
+                break
+            }
+
+            const vizinhos = this.grafo.get(cidadeAtual) || []
+
+            for (const vizinho of vizinhos) {
+                const novaDistancia = distancias.get(cidadeAtual)! + vizinho.custo
+
+                if (novaDistancia < (distancias.get(vizinho.destino) ?? Infinity)) {
+                    distancias.set(vizinho.destino, novaDistancia)
+                    anteriores.set(vizinho.destino, cidadeAtual)
+                }
+
+                fila.push({ 
+                    cidade: vizinho.destino,
+                    custo: novaDistancia
+                })
+            }
+        }
+
+        if (!distancias.has(destino) || distancias.get(destino) === Infinity) {
+            return null
+        }
+
+        const rota: string[] = []
+        let atual: string | null = destino
+        
+        while (atual !== null) {
+            rota.unshift(atual)
+            atual = anteriores.get(atual) || null
+        }
+
+        return {
+            rota,
+            custoTotal: distancias.get(destino)!
+        }
+    }
+}
